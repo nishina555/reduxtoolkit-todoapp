8fd0c6a0539cb751663104265dfb6fa74e3f74d1
nishina555 <n.toshi.1.014@gmail.com>
2022-09-14 20:43:29 +0900

    remove state type index

--- src/types/state/index.ts
+++
@@ -1,7 +0,0 @@
-import { VisibilityFilterTypes } from "./visibilityFilter";
-import { TodoState } from "./todos";
-
-// export type State = {
-//   visibilityFilter: VisibilityFilterTypes;
-//   todos: TodoState;
-// };
