238adc6083878cd0e5dd8565c869ad3500199fb9
nishina555 <n.toshi.1.014@gmail.com>
2022-09-15 18:53:12 +0900

    update

--- src/selectors/todo.ts
+++ src/selectors/todo.ts
@@ -2,14 +2,20 @@ import { AppState } from "../store/index";
 import { VisibilityFilterTypes } from "../types/state/visibilityFilter";
 import { VISIBILITY_FILTERS } from "../types/constants/visibilityFilterType";
 import { TodoState, TodoItem } from "../types/state/todos";
-// import { createSelector } from '@reduxjs/toolkit';
+import { createSelector } from "@reduxjs/toolkit";
 
+// Before
 const todos = (state: AppState): TodoState => state.todos;
-
+const getTodosById = (state: AppState) => todos(state).byId;
 const getTodoIds = (state: AppState): number[] => todos(state).allIds;
-
 const getTodos = (state: AppState): Array<TodoItem> =>
-  getTodoIds(state).map((id) => state.todos.byId[id]);
+  getTodoIds(state).map((id) => getTodosById(state)[id]);
+
+// const getTodosById = createSelector([todos], (todos) => todos.byId);
+
+// const getTodoIds = createSelector([todos], (todos) => todos.allIds);
+
+// const getTodos = createSelector([getTodoIds], (todoIds) => todoIds.map((id) => todos.byId[id]))
 
 export const getTodosByVisibilityFilter = (
   state: AppState,
