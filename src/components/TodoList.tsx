cd2a5fc2672a417ad735edf5d5ca701c6334f674
nishina555 <n.toshi.1.014@gmail.com>
2022-09-16 08:14:30 +0900

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,21 +3,17 @@ import Todo from "./Todo";
 import { AppState } from "../store/index";
 import { getTodosByVisibilityFilter } from "../selectors/todo";
 import { TodoItem } from "../types/state/todos";
-import { connect, useSelector } from "react-redux";
+// import { connect, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styles from "./TodoList.module.css";
-import todosSlice from "../reducers/todosSlice";
+// import todosSlice from "../reducers/todosSlice";
 
-type Props = {
-  todos: TodoItem[];
-};
-
-const TodoList: FC<Props> = ({ todos }) => {
-  // const TodoList: FC = () => {
+const TodoList: FC = () => {
   // Before
-  // const todos: Array<TodoItem> = useSelector((state: AppState) => {
-  //   const { visibilityFilter } = state;
-  //   return getTodosByVisibilityFilter(state, visibilityFilter);
-  // });
+  const todos: Array<TodoItem> = useSelector((state: AppState) => {
+    const { visibilityFilter } = state;
+    return getTodosByVisibilityFilter(state, visibilityFilter);
+  });
 
   // After
   // const todos: Array<TodoItem> = useSelector(getTodosByVisibilityFilter);
@@ -33,11 +29,29 @@ const TodoList: FC<Props> = ({ todos }) => {
   );
 };
 
-const mapStateToProps = (state: AppState): Props => {
-  const { visibilityFilter } = state;
-  const todos = getTodosByVisibilityFilter(state, visibilityFilter);
-  return { todos };
-};
+export default TodoList;
 
-// export default TodoList;
-export default connect(mapStateToProps)(TodoList);
+// connectを利用した実装
+// type Props = {
+//   todos: TodoItem[];
+// };
+
+// const TodoList: FC<Props> = ({ todos }) => {
+//   return (
+//     <ul className={styles.todoList}>
+//       {todos && todos.length
+//         ? todos.map((todo: TodoItem, index: number) => {
+//             return <Todo key={`todo-${todo.id}`} todo={todo} />;
+//           })
+//         : "No todos, yay!"}
+//     </ul>
+//   );
+// };
+
+// const mapStateToProps = (state: AppState): Props => {
+//   const { visibilityFilter } = state;
+//   const todos = getTodosByVisibilityFilter(state, visibilityFilter);
+//   return { todos };
+// };
+
+// export default connect(mapStateToProps)(TodoList);
