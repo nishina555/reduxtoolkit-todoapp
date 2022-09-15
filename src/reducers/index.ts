947c98190b70a74b11ca638d11c76f491660052c
nishina555 <n.toshi.1.014@gmail.com>
2022-09-15 13:18:41 +0900

    change reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import todosReducer from "./todosSlice";
 import visibilityFilterReducer from "./visibilityFilterSlice";
+import countReducer from "./countSlice";
 
 export default combineReducers({
   todos: todosReducer,
   visibilityFilter: visibilityFilterReducer,
+  count: countReducer,
 });
