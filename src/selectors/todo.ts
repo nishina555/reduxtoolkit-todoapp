f90aadb4950c4e4ab7ae2b6e9aa6e2ec09424d08
nishina555 <n.toshi.1.014@gmail.com>
2022-09-15 18:37:14 +0900

    selector修正

--- src/selectors/todo.ts
+++ src/selectors/todo.ts
@@ -1,18 +1,21 @@
 import { AppState } from "../store/index";
 import { VisibilityFilterTypes } from "../types/state/visibilityFilter";
 import { VISIBILITY_FILTERS } from "../types/constants/visibilityFilterType";
-import { TodoItem } from "../types/state/todos";
+import { TodoState, TodoItem } from "../types/state/todos";
+// import { createSelector } from '@reduxjs/toolkit';
 
-const getTodoIds = (store: AppState): number[] => store.todos.allIds;
+const todos = (state: AppState): TodoState => state.todos;
 
-const getTodos = (store: AppState): Array<TodoItem> =>
-  getTodoIds(store).map((id) => store.todos.byId[id]);
+const getTodoIds = (state: AppState): number[] => todos(state).allIds;
+
+const getTodos = (state: AppState): Array<TodoItem> =>
+  getTodoIds(state).map((id) => state.todos.byId[id]);
 
 export const getTodosByVisibilityFilter = (
-  store: AppState,
+  state: AppState,
   visibilityFilter: VisibilityFilterTypes
 ): TodoItem[] => {
-  const allTodos = getTodos(store);
+  const allTodos = getTodos(state);
   console.log("getTodosByVisibilityFilter");
   switch (visibilityFilter) {
     case VISIBILITY_FILTERS.COMPLETED:
