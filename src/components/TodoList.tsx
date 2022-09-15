6a2b7a430f609c63c1be8162ecbc55f11ee6a608
nishina555 <n.toshi.1.014@gmail.com>
2022-09-15 19:55:51 +0900

    update

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Todo from "./Todo";
-import { AppState } from "../store/index";
+// import { AppState } from "../store/index";
 import { getTodosByVisibilityFilter } from "../selectors/todo";
 import { TodoItem } from "../types/state/todos";
 
@@ -8,13 +8,13 @@ import { useSelector } from "react-redux";
 
 const TodoList: React.FC = () => {
   // Before
-  const todos: Array<TodoItem> = useSelector((state: AppState) => {
-    const { visibilityFilter } = state;
-    return getTodosByVisibilityFilter(state, visibilityFilter);
-  });
+  // const todos: Array<TodoItem> = useSelector((state: AppState) => {
+  //   const { visibilityFilter } = state;
+  //   return getTodosByVisibilityFilter(state, visibilityFilter);
+  // });
 
   // After
-  // const todos: Array<TodoItem> = useSelector(getTodosByVisibilityFilter);
+  const todos: Array<TodoItem> = useSelector(getTodosByVisibilityFilter);
 
   return (
     <ul className="todo-list">
